2102356ebfb91140dd34dba127f13ce65dbb9a15
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2019-11-28 19:13:06 +0700

    добавлена версия v0.1.0

--- docker/run-push-image.sh
+++ docker/run-push-image.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 docker push mlrdchain/web-srv:latest
+docker tag mlrdchain/web-srv mlrdchain/web-srv:v0.1.0
+docker push mlrdchain/web-srv:v0.1.0
+docker push mlrdchain/web-srv:latest
 exit 0
