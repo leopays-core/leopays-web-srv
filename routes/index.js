3f6a2045b1cd30b16464f245745f7fc421685eef
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2020-03-03 14:35:21 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.use('/explorer', require('./explorer'));
 router.use('/privacy', require('./privacy'));
 router.use('/terms', require('./terms'));
 // https://auth0.com/docs
-//router.use('/.well-known', require('./.well-known'));
+router.use('/.well-known', require('./.well-known'));
 
 //router.use('/auth', require('./auth'));
 router.use('/api', require('./api'));
