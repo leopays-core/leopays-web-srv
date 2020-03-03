658e92e1e5de6031bfe2773c7bf8a290859321dd
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2020-03-03 14:31:00 +0700

    Update index.js

--- routes/.well-known/index.js
+++ routes/.well-known/index.js
@@ -8,14 +8,4 @@ router.use('/jwks.json', require('./jwks'));
 router.use('/openid-configuration', require('./openid-configuration'));
 router.use('/acme-challenge', require('./acme-challenge'));
 
-'/acme-challenge/7gqsQSvU8jE29zyqP0xISJztw_vBjXzvz1D_oN_X99g'
 module.exports = router;
-/*
-Create a file containing just this data:
-
-7gqsQSvU8jE29zyqP0xISJztw_vBjXzvz1D_oN_X99g.pZ3vjRmDs3wFsuLlAKMfECnOxFhl4Hwg2TDAw_0oKBE
-
-And make it available on your web server at this URL:
-
-http://testnet.milliard.money/.well-known/acme-challenge/7gqsQSvU8jE29zyqP0xISJztw_vBjXzvz1D_oN_X99g
-*/
